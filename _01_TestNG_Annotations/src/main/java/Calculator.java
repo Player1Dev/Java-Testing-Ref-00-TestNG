122ab6be9458b8247c2b1107fce7648c016371db
Atomdevnp <mukeshdevnp@gmail.com>
2016-08-16 08:32:46 +0545

    annotation example added

---
+++ _01_TestNG_Annotations/src/main/java/Calculator.java
@@ -0,0 +1,18 @@
+/**
+ * Created by MM on 8/15/2016.
+ */
+public class Calculator {
+
+    public double add(double a, double b) {
+        return a + b;
+    }
+
+    public double subtract(double a, double b) {
+        return a - b;
+    }
+
+    public double multiply(double a, double b) {
+        return a * b;
+    }
+
+}
